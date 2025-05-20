e3f5252e99476d042ff871e1fb2bad2eb7cbf875
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-05-20 15:21:59 -0300

    Create User

---
+++ src/main/java/br/com/compass/ecommerce_api/entities/User.java
@@ -0,0 +1,54 @@
+package br.com.compass.ecommerce_api.entities;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import br.com.compass.ecommerce_api.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 6)
+    private UserRole role = UserRole.CLIENT;
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+}
---
+++ src/main/java/br/com/compass/ecommerce_api/enums/UserRole.java
@@ -0,0 +1,7 @@
+package br.com.compass.ecommerce_api.enums;
+
+public enum UserRole {
+
+    ADMIN,
+    CLIENT
+}
